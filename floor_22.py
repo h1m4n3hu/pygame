77b3a47c66cfce14933ee2df268de3d61457c2b8
Himanshu Soni <57286608+himanshusoni2708@users.noreply.github.com>
2020-04-08 12:34:46 +0530

    Add files via upload

---
+++ floor_22.py
@@ -0,0 +1,91 @@
+import pygame
+
+pygame.init()
+
+screen=pygame.display.set_mode((1200,500))
+title=pygame.display.set_caption("FirstGame")
+clock=pygame.time.Clock()
+allsprites=pygame.sprite.Group()
+enemies=pygame.sprite.Group()
+vec=pygame.math.Vector2
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.Surface((50,50))
+        self.image.fill((255,0,0))
+        self.rect=self.image.get_rect()
+        self.pos=vec(300,375)
+        self.vel=vec(0,0)
+        self.acc=vec(0,0)
+    def update(self):        
+        self.vel+=self.acc
+        self.pos+=self.vel+(self.acc)/2
+        self.rect.center=self.pos
+box=Box()
+allsprites.add(box)
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.Surface((30,30))
+        self.image.fill((100,100,0))
+        self.rect=self.image.get_rect()
+        self.pos=vec(1300,375)
+        self.vel=vec(0,0)
+        self.acc=vec(0,0)
+    def update(self):   
+        self.vel.x=-3
+        self.vel+=self.acc
+        self.pos+=self.vel+(self.acc)/2
+        self.rect.center=self.pos
+enemy=Enemy()
+allsprites.add(enemy)
+enemies.add(enemy)
+
+class Floor(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.Surface((1200,100))
+        self.image.fill((0,255,0))
+        self.rect=self.image.get_rect()
+        self.pos=vec(600,450)
+    def update(self):        
+        self.rect.center=self.pos
+floor=Floor()
+allsprites.add(floor)
+
+run=True
+while run:
+    
+    if box.pos.x<-50:
+        box.pos.x=1250
+    if box.pos.x>1250:
+        box.pos.x=-50
+    if box.pos.y>375:
+        box.vel.y=0
+        box.acc.y=0
+        
+    col=pygame.sprite.spritecollide(box,enemies,False)
+    if col:
+        print("Collision")
+    
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RIGHT:
+                box.vel.x=3
+            if event.key==pygame.K_LEFT:
+                box.vel.x=-3
+                
+            #jump
+            if event.key==pygame.K_UP:
+                box.vel.y=-3.5
+                box.acc.y=+0.03
+                
+    allsprites.update()
+    screen.fill((0,0,255))
+    allsprites.draw(screen)
+    pygame.display.flip()
+pygame.quit()
